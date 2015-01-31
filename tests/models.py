26fad5c4b644e1b3dca0a714527915c86972a7e4
tonytan4ever <tonytan198211@gmail.com>
2015-01-31 11:11:18 -0500

    Add tests

---
+++ tests/models.py
@@ -0,0 +1,35 @@
+"""Models for test suite."""
+import datetime
+
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Cloud(Base):
+    """A group of machines connected in a cloud."""
+    __tablename__ = 'cloud'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Machine(Base):
+    """A single machine which is part of a cloud deployment."""
+    __tablename__ = 'machine'
+
+    id = db.Column(db.Integer, primary_key=True)
+    hostname = db.Column(db.String, nullable=False)
+    operating_system = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    cloud_id = db.Column(db.Integer, db.ForeignKey('cloud.id'))
+    cloud = db.relationship('Cloud')
+    is_running = db.Column(db.Boolean, default=False, nullable=False)
+    last_started_at = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def __str__(self):
+        """Return string representation."""
+        return self.hostname
