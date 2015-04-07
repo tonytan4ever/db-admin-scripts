ee17f3c621abbc36f824f579569eba832fad11de
tonytan4ever <tonytan198211@gmail.com>
2015-04-07 14:43:31 -0400

    2 SSL Scripts check in

--- convert_backend/get_san_dns.py
+++ convert_backend/get_san_dns.py
@@ -1,4 +1,3 @@
-import json
 import ssl
 import sys
 
@@ -20,7 +19,11 @@ if sslv2 is not None:            # pragma: no cover
     ssl_versions.append(sslv2)   # pragma: no cover
 
 
-def get_remote_cert(remote_host):
+def get_ssl_number_of_hosts(remote_host):
+    '''Get number of Alternative names for a (SAN) Cert
+
+    '''
+
     for ssl_version in ssl_versions:
         try:
             cert = ssl.get_server_certificate((remote_host, 443),
@@ -28,20 +31,30 @@ def get_remote_cert(remote_host):
         except ssl.SSLError:
             # This exception m
             continue
+
         x509 = crypto.load_certificate(crypto.FILETYPE_PEM, cert)
 
-        result = {
-            "pemEncodedCert": str(cert),
-            "sha1Fingerprint": x509.digest('sha1').replace(':','')
-        }
+        sans = []
+        for idx in range(0, x509.get_extension_count()):
+            extension = x509.get_extension(idx)
+            if extension.get_short_name() == 'subjectAltName':
+                sans = [san.replace('DNS:', '') for san
+                        in str(extension).split(',')]
+                break
+
+        # We can actually print all the Subject Alternative Names
+        # for san in sans:
+        #    print san
+        result = len(sans)
         break
     else:
         raise ValueError('Get remote host certificate info failed...')
-    return json.dumps(result)
+    return result
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print('Usage: %s <remote_host_you_want_get_cert_on>' % sys.argv[0])
         sys.exit(0)
-    print(get_remote_cert(sys.argv[1]))
+    print("There are %s DNS names for SAN Cert on %s" % (
+        get_ssl_number_of_hosts(sys.argv[1]), sys.argv[1]))
