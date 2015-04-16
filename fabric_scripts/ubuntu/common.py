cd18f2e98fdad29723a70d8491fa00584bb5e8e3
tonytan4ever <tonytan198211@gmail.com>
2015-04-16 16:56:22 -0400

    Add apt repo

--- fabric_scripts/ubuntu/common.py
+++ fabric_scripts/ubuntu/common.py
@@ -6,5 +6,6 @@ Created on April 15th, 2015
 from fabric.operations import sudo
 
 def apt_get_install(packages):
+    sudo('add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"')
     packages_string = ' '.join(packages)
     sudo('apt-get -y --no-upgrade install %s' % packages_string)
