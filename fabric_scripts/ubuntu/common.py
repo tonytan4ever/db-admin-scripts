5e44fd5702bbdac83773f50de37c4aa0ab230c96
tonytan4ever <tonytan198211@gmail.com>
2015-04-16 16:57:16 -0400

    sudo apt-get update first

--- fabric_scripts/ubuntu/common.py
+++ fabric_scripts/ubuntu/common.py
@@ -7,5 +7,6 @@ from fabric.operations import sudo
 
 def apt_get_install(packages):
     sudo('add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"')
+    sudo('apt-get update')
     packages_string = ' '.join(packages)
     sudo('apt-get -y --no-upgrade install %s' % packages_string)
