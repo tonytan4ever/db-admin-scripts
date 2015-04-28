09886d3cb36ba1beb3e8db91c74490d3c7fab90f
tonytan4ever <tonytan198211@gmail.com>
2015-04-28 15:54:47 -0400

    Update fabfile

--- fabric_scripts/fabfile.py
+++ fabric_scripts/fabfile.py
@@ -1,13 +1,17 @@
 from fabric.api import env
+from fabric.operations import sudo
 import helper
 
-env.hosts = ['root@166.78.111.209']
+env.hosts = ['root@166.78.112.69']
 
 
 def cloud_88_nova_compute_node():
     helper.install_prerequisite()
     helper.install_rabbitmq()
-    helper.install_mysql()
+    helper.install_mysql(username='nova', password='nova',
+                         new_database='nova')
     # python dependencies
     # helper.install_pip()
     # helper.install_python_packages()
+    # c
+    sudo("mkdir /var/log/nova", user="root")
